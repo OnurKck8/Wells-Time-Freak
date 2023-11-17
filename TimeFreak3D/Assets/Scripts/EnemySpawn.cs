3ee6c552372af04038e7bb8fa89e640f2b8e66cc
Onur Küçük <124556360+OnurKck8@users.noreply.github.com>
2023-11-17 15:53:46 +0300

    Added Spawn Barbar

---
+++ TimeFreak3D/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EnemySpawn : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject prefab;
+
+    [SerializeField]
+    private int spawnCount = 10;
+
+    [SerializeField]
+    private float spawnTime;
+
+    [SerializeField]
+    private float spawnDelay;
+
+    [SerializeField]
+    public Vector3[] spawnPoints;
+
+    private int remainingEnemies = 0;
+    int randomSpawnPoint;
+
+    void Start()
+    {
+        StartCoroutine(SpawnObjects());
+        
+    }
+
+    private IEnumerator SpawnObjects()
+    {
+        WaitForSeconds wait = new WaitForSeconds(spawnDelay);
+
+        // Initial wait
+        yield return new WaitForSeconds(spawnTime);
+
+
+        for (int count = spawnCount; count > 0; --count)
+        {
+            randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+            GameObject clone = Instantiate(prefab, spawnPoints[randomSpawnPoint], Quaternion.identity);
+
+            // Detect when an ennemy gets destroyed
+
+
+            remainingEnemies++;
+
+            // Wait before next spawn
+            yield return wait;
+        }
+
+        Debug.Log("All the ennemies have been instantiated !");
+    }
+}
---
+++ TimeFreak3D/Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public GameObject player;
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    public Animator myAnim;
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        if (player == null)
+        {
+            player = GameObject.Find("Third Person Player");
+        }
+    }
+
+    // Update is called once per frame
+    
+    private void FixedUpdate() {
+        Vector3 pos = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(pos);
+        transform.LookAt(player.transform);
+    }
+
+    public void Update()
+    {
+        
+        float distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+        if(distance <= 2f)
+        {
+            myAnim.SetBool("Attack", true);
+            moveSpeed = 0f;
+        }
+        else
+        {
+            myAnim.SetBool("Attack", false);
+            moveSpeed = 5f;
+        }
+           
+    }
+
+}
+
+
